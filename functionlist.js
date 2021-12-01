401bfe135ee6f833e3be3bdd280cb894ff0b36da
Dneifiend <dneifiend@gmail.com>
2021-12-01 17:39:27 +0900

    add function list

---
+++ functionlist.js
@@ -0,0 +1,531 @@
+// melvoridle.com site's onclick function list.
+accountDeletion();
+accountDeletion(true);
+buryItem();
+buryItem(); return false;
+buyMbucks(1);
+buyMbucks(100);
+buyMbucks(1000);
+buyMbucks(25);
+buyMbucks(500);
+castMagic(true);
+changeCombatMenu(0);
+changeCombatMenu(1);
+changeCombatMenu(2);
+changeCombatMenu(3);
+changeEquipToSet(0); return false;
+changeEquipToSet(0); return false;
+changeEquipToSet(0); return false;
+changeEquipToSet(0); return false;
+changeEquipToSet(1); return false;
+changeEquipToSet(1); return false;
+changeEquipToSet(2); return false;
+changeEquipToSet(2); return false;
+changeName();
+changePage(0);
+changePage(1);
+changePage(1, false, false, true);
+changePage(10);
+changePage(11);
+changePage(13);
+changePage(13, false, false); this.blur();
+changePage(13, false, true, false, 12);
+changePage(13, false, true, false, 16);
+changePage(13, false, true, false, 17);
+changePage(13, false, true, false, 18);
+changePage(13, false, true, false, 6);
+changePage(13, false, true, false, 7);
+changePage(13, false, true, false, 8);
+changePage(13, false, true, false, 9);
+changePage(14);
+changePage(15);
+changePage(16);
+changePage(17);
+changePage(18);
+changePage(19);
+changePage(2);
+changePage(23);
+changePage(24);
+changePage(26);
+changePage(28);
+changePage(29);
+changePage(3);
+changePage(30);
+changePage(30); showCompletionCategory(0);
+changePage(30); showCompletionCategory(1);
+changePage(30); showCompletionCategory(2);
+changePage(30); showCompletionCategory(3);
+changePage(30); showCompletionCategory(4);
+changePage(31);
+changePage(6);
+changePage(7);
+changePage(8);
+changePage(9);
+changePageCharacterSelection(0);
+changePageCharacterSelection(0);
+changePageCharacterSelection(0);
+changePageCharacterSelection(1);
+changePageCharacterSelection(1);
+changePageCharacterSelection(1);
+changePageCharacterSelection(1);
+changePageCharacterSelection(2);
+changePageCharacterSelection(3);
+changePageCharacterSelection(3);
+changePageCharacterSelection(6);
+changePageCharacterSelection(7);
+changePageCharacterSelection(7);
+changePageCharacterSelection(9);
+changePageCharacterSelection(9);
+changeSetting(17, 0)
+changeSetting(17, 1)
+changeSetting(2, 0)
+changeSetting(2, 10)
+changeSetting(2, 11)
+changeSetting(2, 13)
+changeSetting(2, 14)
+changeSetting(2, 15)
+changeSetting(2, 16)
+changeSetting(2, 17)
+changeSetting(2, 18)
+changeSetting(2, 19)
+changeSetting(2, 2)
+changeSetting(2, 26)
+changeSetting(2, 28)
+changeSetting(2, 31)
+changeSetting(2, 7)
+changeSetting(2, 8)
+changeSetting(2, 9)
+changeSetting(21, 0)
+changeSetting(21, 1)
+changeSetting(42, 0)
+changeSetting(42, 1)
+changeSetting(43);
+changeSetting(44);
+changeSetting(45);
+changeSetting(46);
+changeSetting(47);
+changeSetting(8, false);
+changeSetting(8, true);
+changeSettingsToggle(11);
+changeSettingsToggle(15);
+changeSettingsToggle(27);
+changeSettingsToggle(27);
+changeSettingsToggle(30);
+changeSettingsToggle(30);
+changeSettingsToggle(32);
+changeSettingsToggle(48); changePage(9, false, false);
+claimToken();
+claimToken();
+clearBankSearch();
+combatManager.player.equipCallback(320, 'Ring');
+combatManager.player.equipCallback(320, 'Ring');
+combatManager.resumeDungeon();
+confirmLoadLocalSave();
+confirmMoveModeSelection();
+confirmSellModeSelection();
+connectDevicePushNotifications();
+convertSave();
+createAstrologySelectedUI(0);
+createAstrologySelectedUI(1);
+createAstrologySelectedUI(10);
+createAstrologySelectedUI(2);
+createAstrologySelectedUI(3);
+createAstrologySelectedUI(4);
+createAstrologySelectedUI(5);
+createAstrologySelectedUI(6);
+createAstrologySelectedUI(7);
+createAstrologySelectedUI(8);
+createAstrologySelectedUI(9);
+destroyAgilityObstacle(0);
+disconnectDevicePushNotifications();
+dismissOfflineCombatAlert();
+dismissOfflineThievingAlert();
+displaySelectAgilityObstacle(0);
+downloadSave()
+downloadSave()
+downloadSave();
+enableOfflineCombat();
+enableOfflineThieving();
+equipFood(); return false;
+equipFood(); return false;
+exportSave();
+exportSave(true);
+filterSynergyFamiliar(-1);
+filterSynergyFamiliar(-2);
+findAFriend();
+findAFriend();
+forceSync(false, false);
+forgotPasswordMelvorCloud(); return false;
+game.firemaking.burnLog();
+game.firemaking.lightBonfire();
+game.firemaking.renderLogSelection();
+loadCloudSave(true);
+loadFarmingArea(0)
+loadFarmingArea(1)
+loadFarmingArea(2)
+loadGame();
+loadPotions();
+location.reload(true);
+lockItem(); this.blur();
+lockItem(); this.blur();
+lockItem(); this.blur();
+lockItem(); this.blur();
+loginToMelvorCloud(); return false;
+logoutMelvorCloud();
+logoutMelvorCloud();
+openBankItem();
+openBankItem(); return false;
+openCharacterSelectionSettings(0, 'Dneif');
+openCharacterSelectionSettings(1, 'DneifP');
+openDefaultBankTabDropdown();
+openDiscordLink();
+openDiscordLink();
+openImportSave(2);
+openSynergiesBreakdown();
+openSynergiesBreakdown();
+openWikiLink();
+performUnlockIAP();
+plantSeed();
+plantSeed(true);
+processQuickBuyItem();
+quickBuyItem(159);
+quickBuyItem(679);
+quickCreateTablet(0);
+quickCreateTablet(3);
+quickEquipItem(CONSTANTS.item.Max_Skillcape, 15); this.blur();
+quickEquipSkillcape(15); this.blur();
+raidManager.skipWave();
+raidManager.startRaid();
+readPlayFabNews();
+readPlayFabNews();
+registerToMelvorCloud(); return false;
+rerollAstrologyModifiers(0, 'standard');
+rerollAstrologyModifiers(0, 'unique');
+rerollSpecificAstrologyModifier(0, 0, true);
+rerollSpecificAstrologyModifier(0, 1, true);
+rerollSpecificAstrologyModifier(0, 2, true);
+rerollSpecificAstrologyModifier(0, 3, true);
+rerollSpecificAstrologyModifier(0, 4, true);
+rerollSpecificAstrologyModifier(0, 5, true);
+return false;
+saveData(); location.reload();
+selectBankItem(0);
+selectBankItem(1);
+selectBankItem(1035);
+selectBankItem(12);
+selectBankItem(128);
+selectBankItem(13);
+selectBankItem(143);
+selectBankItem(144);
+selectBankItem(151);
+selectBankItem(246);
+selectBankItem(247);
+selectBankItem(25);
+selectBankItem(292);
+selectBankItem(3);
+selectBankItem(320);
+selectBankItem(325);
+selectBankItem(389);
+selectBankItem(390);
+selectBankItem(391);
+selectBankItem(393);
+selectBankItem(395);
+selectBankItem(4);
+selectBankItem(403);
+selectBankItem(439);
+selectBankItem(49);
+selectBankItem(490);
+selectBankItem(51);
+selectBankItem(531);
+selectBankItem(57);
+selectBankItem(58);
+selectCharacter(0); this.blur();
+selectCharacter(0, false, 0)
+selectCharacter(1); this.blur();
+selectCharacter(1, false, 2)
+selectCharacter(2); this.blur();
+selectFish(0, 0);
+selectFish(0, 1);
+selectFish(1, 0);
+selectFish(1, 1);
+selectFish(2, 0);
+selectFish(2, 1);
+selectFish(2, 2);
+selectFish(3, 0);
+selectFish(3, 1);
+selectFish(3, 2);
+selectFish(4, 0);
+selectFish(6, 0);
+selectFish(6, 1);
+selectFish(7, 0);
+selectFish(7, 1);
+selectFletch(0);
+selectSpellbook('ancient');
+selectSpellbook('aurora');
+selectSpellbook('curse');
+selectSpellbook('standard');
+sellItem();
+sellItem();
+setAccount();
+setAccount();
+setDefaultBankSorting(0)
+setDefaultBankSorting(0)
+setDefaultBankSorting(1)
+setDefaultBankSorting(1)
+setDefaultBankSorting(2)
+setDefaultBankSorting(2)
+setDefaultBankSorting(3)
+setDefaultBankSorting(3)
+setDefaultBankSorting(4)
+setDefaultBankSorting(4)
+setDefaultBankSorting(5)
+setDefaultBankSorting(5)
+setGamemode(0); return false;
+setGamemode(1); return false;
+setMoveItemTab(0)
+setMoveItemTab(1)
+setMoveItemTab(2)
+setMoveItemTab(3)
+setMoveItemTab(4)
+setMoveItemTab(5)
+setMoveItemTab(6)
+setMoveItemTab(7)
+setMoveItemTab(8)
+setMoveItemTab(9)
+shopMenu.showAllTabsButRaid();
+shopMenu.showSingleTab('General');
+shopMenu.showSingleTab('Gloves')
+shopMenu.showSingleTab('Materials');
+shopMenu.showSingleTab('SkillUpgrades');
+shopMenu.showSingleTab('Skillcapes');
+shopMenu.showSingleTab('Slayer');
+showAllAgilityPassives();
+showBankTab(0);
+showBankTab(1);
+showBankTab(2);
+showBankTab(3);
+showBankTab(4);
+showBankTab(5);
+showBankTab(6);
+showBankTab(7);
+showBankTab(8);
+showBankTab(9);
+showCombatArea('Combat')
+showCombatArea('Dungeon')
+showCombatArea('Slayer')
+showCompletionCategory(0);
+showCompletionCategory(1);
+showCompletionCategory(2);
+showCompletionCategory(3);
+showCompletionCategory(4);
+showEnableOfflineCombatModal();
+showMasteryUnlocks(0);
+showMasteryUnlocks(1);
+showMasteryUnlocks(10);
+showMasteryUnlocks(11);
+showMasteryUnlocks(13);
+showMasteryUnlocks(14);
+showMasteryUnlocks(15);
+showMasteryUnlocks(15); this.blur();
+showMasteryUnlocks(19);
+showMasteryUnlocks(2);
+showMasteryUnlocks(20);
+showMasteryUnlocks(21);
+showMasteryUnlocks(22);
+showMasteryUnlocks(3);
+showMasteryUnlocks(4);
+showMasteryUnlocks(5);
+showPageLoader(); setLanguage('de'); location.reload();
+showPageLoader(); setLanguage('en'); location.reload();
+showPageLoader(); setLanguage('es'); location.reload();
+showPageLoader(); setLanguage('fr'); location.reload();
+showPageLoader(); setLanguage('it'); location.reload();
+showPageLoader(); setLanguage('ja'); location.reload();
+showPageLoader(); setLanguage('ko'); location.reload();
+showPageLoader(); setLanguage('pt'); location.reload();
+showPageLoader(); setLanguage('pt-br'); location.reload();
+showPageLoader(); setLanguage('ru'); location.reload();
+showPageLoader(); setLanguage('tr'); location.reload();
+showPageLoader(); setLanguage('zh-CN'); location.reload();
+showPageLoader(); setLanguage('zh-TW'); location.reload();
+showSpendMasteryXP(0);
+showSpendMasteryXP(0);
+showSpendMasteryXP(1);
+showSpendMasteryXP(1);
+showSpendMasteryXP(10);
+showSpendMasteryXP(10);
+showSpendMasteryXP(11);
+showSpendMasteryXP(11);
+showSpendMasteryXP(13);
+showSpendMasteryXP(13);
+showSpendMasteryXP(14);
+showSpendMasteryXP(14);
+showSpendMasteryXP(15);
+showSpendMasteryXP(15);
+showSpendMasteryXP(19);
+showSpendMasteryXP(19);
+showSpendMasteryXP(2);
+showSpendMasteryXP(2);
+showSpendMasteryXP(20);
+showSpendMasteryXP(20);
+showSpendMasteryXP(21);
+showSpendMasteryXP(21);
+showSpendMasteryXP(22);
+showSpendMasteryXP(22);
+showSpendMasteryXP(3);
+showSpendMasteryXP(3);
+showSpendMasteryXP(4);
+showSpendMasteryXP(4);
+showSpendMasteryXP(5);
+showSpendMasteryXP(5);
+showUsernameChange()
+skipEmailAndTransitionToPlayFab(); this.blur(); return false;
+skipEmailAndTransitionToPlayFab(); this.blur(); return false;
+skipTutorial();
+sortBank();
+startAgility();
+startAgility(0, true, false);
+submitEmailAndTransitionToPlayFab(); this.blur(); return false;
+submitEmailAndTransitionToPlayFab(); this.blur(); return false;
+testLoginAndTransitionToPlayFab(); this.blur();
+testLoginAndTransitionToPlayFab(); this.blur();
+this.setSelectionRange(0, this.value.length)
+this.setSelectionRange(0, this.value.length)
+this.setSelectionRange(0, this.value.length)
+toggleAstrology(0);
+toggleAstrology(1);
+toggleAstrology(10);
+toggleAstrology(2);
+toggleAstrology(3);
+toggleAstrology(4);
+toggleAstrology(5);
+toggleAstrology(6);
+toggleAstrology(7);
+toggleAstrology(8);
+toggleAstrology(9);
+toggleBankBorders();
+toggleBankBorders();
+toggleCharacterSelectionView();
+toggleCharacterSelectionView(); return false;
+toggleCombatSkillMenu();
+toggleMenu(0);
+toggleMenu(1);
+toggleMoveItemMode();
+toggleOfflineCombatCheckbox(0);
+toggleOfflineCombatCheckbox(1);
+toggleOfflineCombatCheckbox(2);
+toggleOfflineCombatCheckbox(3);
+toggleOfflineCombatCheckbox(4);
+toggleSellItemMode();
+toggleSkillMinibar(); this.blur();
+updateEmailMelvorCloud(); return false;
+updateMilestoneTab(0);
+updateMilestoneTab(0);
+updateMilestoneTab(1);
+updateMilestoneTab(1);
+updateMilestoneTab(10);
+updateMilestoneTab(10);
+updateMilestoneTab(11);
+updateMilestoneTab(11);
+updateMilestoneTab(12);
+updateMilestoneTab(12);
+updateMilestoneTab(13);
+updateMilestoneTab(13);
+updateMilestoneTab(14);
+updateMilestoneTab(14);
+updateMilestoneTab(15);
+updateMilestoneTab(15);
+updateMilestoneTab(15); this.blur();
+updateMilestoneTab(16);
+updateMilestoneTab(16);
+updateMilestoneTab(17);
+updateMilestoneTab(17);
+updateMilestoneTab(18);
+updateMilestoneTab(18);
+updateMilestoneTab(19);
+updateMilestoneTab(19);
+updateMilestoneTab(2);
+updateMilestoneTab(2);
+updateMilestoneTab(20);
+updateMilestoneTab(20);
+updateMilestoneTab(21);
+updateMilestoneTab(21);
+updateMilestoneTab(22);
+updateMilestoneTab(3);
+updateMilestoneTab(3);
+updateMilestoneTab(4);
+updateMilestoneTab(4);
+updateMilestoneTab(5);
+updateMilestoneTab(5);
+updateMilestoneTab(6);
+updateMilestoneTab(6);
+updateMilestoneTab(7);
+updateMilestoneTab(7);
+updateMilestoneTab(8);
+updateMilestoneTab(8);
+updateMilestoneTab(9);
+updateMilestoneTab(9);
+updatePasswordMelvorCloud(); return false;
+updateSellQty(0); return false;
+updateSellQty(0); return false;
+updateSellQty(1); return false;
+updateSellQty(1); return false;
+updateSynergyFamiliarFilter();
+upgradeItem();
+upgradeItem(); return false;
+useEight();
+useEight(); return false;
+viewAllAstrologyModifiers();
+viewEquipmentStats();
+viewEquipmentStats();
+viewGameGuide();
+viewItemContents();
+viewItemContents();
+viewItemStats(-1); return false;
+viewItemStats(-1); return false;
+viewItemStats(-1); return false;
+viewItemStats(-1); return false;
+viewMasteryCheckpoints(0);
+viewMasteryCheckpoints(1);
+viewMasteryCheckpoints(10);
+viewMasteryCheckpoints(11);
+viewMasteryCheckpoints(13);
+viewMasteryCheckpoints(14);
+viewMasteryCheckpoints(15);
+viewMasteryCheckpoints(19);
+viewMasteryCheckpoints(2);
+viewMasteryCheckpoints(20);
+viewMasteryCheckpoints(21);
+viewMasteryCheckpoints(22);
+viewMasteryCheckpoints(3);
+viewMasteryCheckpoints(4);
+viewMasteryCheckpoints(5);
+viewMonsterDrops(-1, true)
+viewMonsterStats(111);
+viewMonsterStats(14);
+viewMonsterStats(35);
+viewMonsterStats(37);
+viewMonsterStats(38);
+viewMonsterStats(39);
+viewMonsterStats(40);
+viewMonsterStats(59);
+viewMonsterStats(67);
+viewMonsterStats(70);
+viewMonsterStats(75);
+viewMonsterStats(81);
+viewMonsterStats(9);
+viewPossibleModifiers(0)
+viewPossibleModifiers(1)
+viewPossibleModifiers(10)
+viewPossibleModifiers(2)
+viewPossibleModifiers(3)
+viewPossibleModifiers(4)
+viewPossibleModifiers(5)
+viewPossibleModifiers(6)
+viewPossibleModifiers(7)
+viewPossibleModifiers(8)
+viewPossibleModifiers(9)
+void (0)
+void (0)
+void (0)
+window.open('https://www.reddit.com/r/MelvorIdle/comments/q9rkd0/important_changes_to_mobile_and_web_versions_of/', '_blank').focus();
