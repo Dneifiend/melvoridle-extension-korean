b9b27764699dbe18b7d516f217978489e3528a4e
Dneifiend <dneifiend@gmail.com>
2021-12-02 12:54:30 +0900

    [개선] autofood 구조

--- autofood.js
+++ autofood.js
@@ -54,26 +54,21 @@ class AutoFight {
     autoFood() {
         this.#autofoodInterval = setInterval(() => {
             var eatCount = Math.floor((this.maxHealth - this.currentHealth) / this.currentFoodRestoreValue)
+            if (eatCount === 0) return
 
             // 음식 갯수가 eatCount보자 모자르면 전투 중단
             if (this.foodcount == 0 || this.foodcount < eatCount) {
+                this.off
                 clearInterval(this.#autofoodInterval)
 
+                game.combat.stopCombat()
+                game.thieving.stop()
+
                 alert("음식이 부족하여 전투/절도를 중단합니다.")
-                if (game.combat.isInCombat) {
-                    game.combat.stopCombat()
-                }
-                if (game.thieving.isActive) {
-                    game.thieving.stop()
-                }
                 return
             }
 
-            if (eatCount > 0) {
-                // console.log(eatCount)
-                player.eatFood(eatCount, false)
-            }
-
+            player.eatFood(eatCount, false)
 
         }, 100);
     }
