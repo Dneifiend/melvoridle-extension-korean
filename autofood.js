1783a10bb877fc804dc6e992327382c40ac3fa6e
Dneifiend <dneifiend@gmail.com>
2021-12-02 20:20:14 +0900

    [add] autofood 음식슬롯 자동 변경

--- autofood.js
+++ autofood.js
@@ -56,13 +56,21 @@ class AutoFight {
             var eatCount = Math.floor((this.maxHealth - this.currentHealth) / this.currentFoodRestoreValue)
             if (eatCount === 0) return
 
-            // 음식 갯수가 eatCount보자 모자르면 전투 중단
+
+            // 현재 선택한 음식 갯수가 0이면 다른 슬롯에서 음식을 찾아서 선택한다.
+            if (this.foodcount === 0) {
+                for (let i = 0; i < player.food.slots.length; i++) {
+                    if (player.food.slots[i].quantity > 0) {
+                        return player.selectFood(i);
+                    }
+                }
+            }
+
+            // 음식 갯수가 eatCount보다 부족하면 전투를 중지한다.
             if (this.foodcount == 0 || this.foodcount < eatCount) {
                 this.off
-
                 game.combat.stopCombat()
                 game.thieving.stop()
-
                 alert("음식이 부족하여 전투/절도를 중단합니다.")
                 return
             }
